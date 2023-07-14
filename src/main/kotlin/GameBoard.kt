ce8ee446b888b3c7bde0c060c8fb17fa6801c113
locolin <locolin99@gmail.com>
2023-07-15 00:34:45 +0800

    finish implement 1v1 tic tac toe using OOP

--- src/main/kotlin/GameBoard.kt
+++ src/main/kotlin/GameBoard.kt
@@ -1,23 +1,64 @@
 class GameBoard {
-    var board: IntArray = IntArray(9)
+    private var board: Array<Int?> = arrayOfNulls(9)
 
-    fun checkIfWin() {
+    fun checkIfWin(player: Player): Boolean {
+        for (i in 0..2){
+            if (board[i] == board[i+1] && board[i] == board[i+2] && board[i] == player.playerNum) return true
+            if (board[i] == board[i+3] && board[i] == board[i+6] && board[i] == player.playerNum) return true
+        }
 
+        if (board[0] == board[4] && board[0] == board[8] && board[0] == player.playerNum) return true
+        if (board[2] == board[4] && board[2] == board[6] && board[2] == player.playerNum) return true
+
+        return false
     }
 
-    fun checkIfDraw() {
-
+    fun checkIfDraw(): Boolean {
+        board.forEach{
+            if (it == null) return false
+        }
+        return true
     }
 
-    fun getPlayerInput() {
-
+    fun getPlayerInput(player: Player): Int{
+        print("Player ${player.playerNum}, Input current move [ ${player.symbol} ]: ")
+        var input: Int = readLine()!!.toInt()
+        while (!isValidateInput(input)) {
+            print("Input move again: ")
+            input = readLine()!!.toInt()
+        }
+        return input
     }
 
-    fun printBoard() {
+    fun placeInputToBoard(player: Player, input: Int) {
+        this.board[input] = player.playerNum
+    }
+
+    private fun isValidateInput(input: Int): Boolean{
+        if (input < 0 || input > 8){
+            println("Out of Range!")
+            return false
+        }
+
+        board[input]?.let{
+            println("It is occupied!")
+            return false
+        }
+
+        return true
+    }
+
+    fun printBoard(player1: Player, player2: Player) {
         println("—————————————")
         for (i in 0..2) {
             for (j in 0..2) {
-                print("| ${i * 3 + j} ")
+                val cell = this.board[i * 3 + j]?.let {
+                    if (this.board[i * 3 + j] == player1.playerNum) player1.symbol
+                    else player2.symbol
+                } ?: run {
+                    i * 3 + j
+                }
+                print("| $cell ")
             }
             print("|\n")
             println("—————————————")
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,25 @@ fun main(){
     var gameBoard = GameBoard()
     var player1 = Player(1, "O")
     var player2 = Player(2, "X")
+    var curPlayer = player1
 
-    gameBoard.printBoard()
+    gameBoard.printBoard(player1, player2)
+
+    while(true){
+        val input = gameBoard.getPlayerInput(curPlayer)
+
+        gameBoard.placeInputToBoard(curPlayer, input)
+        gameBoard.printBoard(player1, player2)
+
+        if (gameBoard.checkIfWin(curPlayer)) {
+            println("Congratulations! Player ${curPlayer.playerNum} has won!")
+            break
+        }
+        if (gameBoard.checkIfDraw()) {
+            println("Game is draw!")
+            break
+        }
+
+        curPlayer = if (curPlayer == player1) player2 else player1
+    }
 }
